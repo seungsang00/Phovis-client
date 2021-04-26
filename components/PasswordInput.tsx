06e0f4dd358011ea7a5ed066f3e6b7db72a3cc10
EonJo_Mac <sim04170@gmail.com>
2021-04-26 21:24:11 +0900

    Add: PasswordInput component

---
+++ components/PasswordInput.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+type Props = {
+  label?: string
+  placeholder?: string
+  name: string
+  value: string
+  onChange: (e?: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const PasswordInput = ({
+  label,
+  placeholder,
+  name,
+  value,
+  onChange,
+}: Props) => {
+  const [viewPassword, setViewPassword] = useState(false)
+  const passwordVisibleHandler = () => {
+    setViewPassword(!viewPassword)
+  }
+
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <div>
+        <input
+          type={viewPassword ? 'text' : 'password'}
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        <div onClick={passwordVisibleHandler}>Eye Icon</div>
+      </div>
+    </div>
+  )
+}
+
+export default PasswordInput
