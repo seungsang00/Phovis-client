d04d20c04a879a8a60eab9746f63b32b4e0349ad
EonJo_Mac <sim04170@gmail.com>
2021-04-26 17:54:56 +0900

    Add: LableTextInput component

---
+++ components/LableTextInput.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+type Props = {
+  label?: string
+  placeholder?: string
+  name: string
+  value: string
+  onChange: (e?: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const LableTextInput = ({
+  label,
+  placeholder,
+  name,
+  value,
+  onChange,
+}: Props) => (
+  <div>
+    <label htmlFor={name}>{label}</label>
+    <input
+      type='text'
+      id={name}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  </div>
+)
+
+export default LableTextInput
