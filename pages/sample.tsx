6d9b25738e00152da498152a8c39184a38fd89e1
jeonghun-project <dsjoh111@gmail.com>
2021-04-26 20:56:02 +0900

    Abb Like Atomic Components
    new Atomix Components add Like component
    
    close #21

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,15 +1,24 @@
+import Link from 'next/link'
 import Layout from '../components/Layout'
 import TabMenu from '../components/TabMenu'
 import LocationInfo from '../components/LocationInfo'
+import Like from '../components/Like'
 
 const ComponentSamplePage = () => (
   <Layout title='Component Sample | Next.js + TypeScript Example'>
     <h1>Component Sample</h1>
     <p>This is the Component Sample page</p>
-    {/* <Test text='Hello Next.js' /> */}
     <hr />
+    {/* 여기에 새로 생성한 컴포넌트들을 배치해주세요 */}
     <TabMenu isOwner={true} />
     <LocationInfo location={'서울시 강서구 서울식물원'} />
+    <Like like={23} />
+    <hr />
+    <p>
+      <Link href='/'>
+        <a>Go home</a>
+      </Link>
+    </p>
   </Layout>
 )
 
