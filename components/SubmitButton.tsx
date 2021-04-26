5e1758963cd90a53e1993f6f3f8ee58370920cd4
EonJo_Mac <sim04170@gmail.com>
2021-04-26 23:25:00 +0900

    Update: SubmitButton, modify button event property type

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -3,7 +3,7 @@ import { Button } from '../styles/common'
 
 type Props = {
   text?: string
-  onSubmit: (e: React.MouseEvent<HTMLButtonElement>) => void
+  onSubmit: () => void
 }
 
 // TODO : 버튼에 아이콘 추가 해야 됨
