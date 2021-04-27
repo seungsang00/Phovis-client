72558dad4221bbf6794c32f9f1d9ad6df2e45226
Seungyeon Yoo <y2s9310@gmail.com>
2021-04-27 14:24:13 +0900

    Update Tag components

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -5,10 +5,10 @@ interface IProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void // TODO: 클릭 시 태그 검색 페이지로 이동
 }
 
-export const TagBig = (props: IProps) => (
-  <TagB onClick={props.onClick}># {props.tagname}</TagB>
+export const TagBig = ({ tagname, onClick }: IProps) => (
+  <TagB onClick={onClick}># {tagname}</TagB>
 )
 
-export const TagSmall = (props: IProps) => (
-  <TagS onClick={props.onClick}># {props.tagname}</TagS>
+export const TagSmall = ({ tagname, onClick }: IProps) => (
+  <TagS onClick={onClick}># {tagname}</TagS>
 )
