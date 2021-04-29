bba36826ef72994f8872cf7846de82c136df7337
Seungyeon Yoo <y2s9310@gmail.com>
2021-04-29 21:58:54 +0900

    Fix: build error
    - 불필요한 파일 삭제
    - 사용되지 않는 코드 주석처리

--- pages/api/location/index.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-// 키워드 검색으로 좌표를 받아와서
-
-// 받아온 좌표를 중심으로 지도를 띄우고
-
-// 지도에 마커를 찍어서 위치정보 설정
--- pages/auth/kakao.tsx
+++ pages/auth/kakao.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
 type Props = {
   location?: string
