a03b30ce3944eab7f400c2313f68cf7f9255b534
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-02 17:54:58 +0900

    Add: content page

---
+++ containers/ContentBanner/index.tsx
@@ -0,0 +1,7 @@
+import { DivWithBgImg } from '@styles/index'
+
+const ContentBanner = ({ mainImgUrl }) => {
+  return <DivWithBgImg bgUrl={mainImgUrl}>banner</DivWithBgImg>
+}
+
+export default ContentBanner
---
+++ pages/content/[content_id].tsx
@@ -0,0 +1,30 @@
+// "/content/:content_id"
+import Layout from '@components/Layout'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+// TODO: content_id로 서버에 데이터를 요청합니다
+const ContentPage = () => {
+  const router = useRouter()
+  const { content_id } = router.query
+
+  useEffect(() => {
+    console.log(router.query)
+  })
+  return (
+    <>
+      <Head>
+        <title>Phovis - Content title</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <section>content banner</section>
+      <main>content main</main>
+      <section>연관 출장글</section>
+      <section>photocards</section>
+    </>
+  )
+}
+
+export default ContentPage
