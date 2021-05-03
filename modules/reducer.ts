949d59409879f27da2fa4f96ef34f3758934f782
Eon Jo - Sim <34697086+sim0417@users.noreply.github.com>
2021-05-03 22:57:33 +0900

    Update reducer.ts
    solve conflict

--- modules/reducer.ts
+++ modules/reducer.ts
@@ -3,11 +3,7 @@ import user from './users'
 import main from './main'
 import content from './content'
 
-<<<<<<< HEAD
-const rootReducer = combineReducers({ user, main })
-=======
 const rootReducer = combineReducers({ user, main, content })
->>>>>>> dev
 
 // 루트 리듀서를 내보내주세요.
 export default rootReducer
