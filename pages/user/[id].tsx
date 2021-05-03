39f897b867f1d7096618b4aef84fdbebe9503c53
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-04 00:18:10 +0900

    Add: Tab component

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,4 +1,5 @@
 import { CommonLayout } from '@containers/Layout'
+import { Tab } from '@components/index'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
@@ -12,7 +13,7 @@ const UserPage = () => {
       console.log(`user id >>`, user_id)
     }
   }, [user_id])
-
+  const tabList = ['Content', 'Likes', 'Bookmark']
   return (
     <>
       <Head>
@@ -20,7 +21,12 @@ const UserPage = () => {
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <CommonLayout>{/* 탭이 들어갈 예정입니다 */}</CommonLayout>
+      <CommonLayout>
+        <>
+          <Tab tablist={tabList} />
+          {/* 여기에 탭 메뉴에 해당하는 내용을 넣어주세요. 동적 라우트로 처리하는게 좋을까요? */}
+        </>
+      </CommonLayout>
     </>
   )
 }
--- pages/user/mypage.tsx
+++ pages/user/mypage.tsx
@@ -1,7 +1,9 @@
 import { CommonLayout } from '@containers/Layout'
+import { Tab } from '@components/index'
 import Head from 'next/head'
 
 const MyPage = () => {
+  const tabList = ['Content', 'Likes', 'Bookmark', 'Setting']
   return (
     <>
       <Head>
@@ -9,7 +11,12 @@ const MyPage = () => {
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <CommonLayout>{/* 탭이 들어갈 예정입니다 */}</CommonLayout>
+      <CommonLayout>
+        <>
+          <Tab tablist={tabList} />
+          {/* 여기에 탭 메뉴에 해당하는 내용을 넣어주세요. 동적 라우트로 처리하는게 좋을까요? */}
+        </>
+      </CommonLayout>
     </>
   )
 }
