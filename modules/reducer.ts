e4d2d5fb36ee2c19c3af47f19c8c7e8ff321ae78
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-03 14:08:38 +0900

    Remove: content module in reducer

--- modules/reducer.ts
+++ modules/reducer.ts
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux'
 import user from './users'
-import content from './content'
 import main from './main'
 
-const rootReducer = combineReducers({ user, content, main })
+const rootReducer = combineReducers({ user, main })
 
 // 루트 리듀서를 내보내주세요.
 export default rootReducer
