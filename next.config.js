f23df9290a2f03bfd0a92e9f91876fbe21ba50f5
Jaewon <j901207@gmail.com>
2021-05-06 14:41:20 +0900

    Fix: config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
     loader: 'imgix',
     domain: ['phovis.ga'],
   },
+  env: {
+    NEXT_PUBLIC_DOMAIN: 'https://phovis.ga',
+    NEXT_PUBLIC_API_ENDPOINT: 'https://api.phovis.ga',
+  },
 }
